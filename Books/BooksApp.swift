076b501ef9969debaec319e2f34f2e24308a1806
torekhanmukhtarov <nahkerot@gmail.com>
2024-03-25 14:39:09 +0500

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Torekhan Mukhtarov on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
